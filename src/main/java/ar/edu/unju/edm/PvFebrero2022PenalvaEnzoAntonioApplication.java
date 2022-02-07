22995662ac53a070d57ace8df96b77875a9720d0
enzo2 <enzo2@192.168.0.4>
2022-02-07 20:49:23 -0300

    nothing

---
+++ src/main/java/ar/edu/unju/edm/PvFebrero2022PenalvaEnzoAntonioApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvFebrero2022PenalvaEnzoAntonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvFebrero2022PenalvaEnzoAntonioApplication.class, args);
+	}
+
+}

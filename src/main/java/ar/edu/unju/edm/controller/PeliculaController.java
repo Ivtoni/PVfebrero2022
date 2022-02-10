a2c327f6946c3878b8e569a549e142d217bc0909
enzo2 <enzo2@192.168.0.4>
2022-02-10 17:38:49 -0300

    Corrijo un error de la clase PeliculaController

--- src/main/java/ar/edu/unju/edm/controller/PeliculaController.java
+++ src/main/java/ar/edu/unju/edm/controller/PeliculaController.java
@@ -1,5 +1,63 @@
 package ar.edu.unju.edm.controller;
 
-public class PeliculaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import ar.edu.unju.edm.model.Pelicula;
+import ar.edu.unju.edm.services.IPeliculaService;
+
+@Controller
+public class PeliculaController {
+	@Autowired
+	IPeliculaService unaPeli;
+	@GetMapping ("/pelicula") 
+	public String cargarPelicula(Model model) {
+		model.addAttribute("lista", unaPeli.obtenerTodasPelis());	
+		model.addAttribute("unaPeli", unaPeli.crearPeli() );
+			return "pelicula";
+}
+	@PostMapping("/pelicula/guardar")
+	public String guardarPelicula(@ModelAttribute ("unCliente") Pelicula unaPelicula, Model model) {
+		unaPeli.guardarPeli(unaPelicula);
+		return "redirect:/cliente";
+}
+	@GetMapping("/pelicula/editar/{codPeli}")
+	public String editarPelicula(Model model, @PathVariable(name="codPeli") int codPeli) throws Exception {
+		try {
+			Pelicula peliculaEncontrada = unaPeli.encontrarPeli(codPeli);
+			model.addAttribute("unaPeli", peliculaEncontrada);	
+			model.addAttribute("editMode", "true");
+		}
+		catch (Exception e) {
+			model.addAttribute("formUsuarioErrorMessage",e.getMessage());
+			model.addAttribute("unaPeli", unaPeli.crearPeli());
+			model.addAttribute("editMode", "false");
+		}
+		model.addAttribute("lista", unaPeli.obtenerTodasPelis());
+		return("cliente");
+	}
+	@PostMapping ("/pelicula/modificar")
+	public String modificarPelicula(@ModelAttribute("unaPeli") Pelicula unaPelicula, Model model) {
+		try {
+			unaPeli.modificarPeli(unaPelicula);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "redirect:/cliente";
+	}
+	@GetMapping("/pelicula/eliminar/{codPeli}")
+	public String eliminarPelicula(Model model, @PathVariable(name="codPeli") int codPeli) {
+		try {			unaPeli.eliminarPeli(codPeli);			
+		}
+		catch(Exception e){
+			model.addAttribute("listErrorMessage",e.getMessage());
+		}			
+		return "redirect:/pelicula";
+	}
 }
